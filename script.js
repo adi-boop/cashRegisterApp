e9cf0505725c80512bb9445515fb2ee847248243
Aditya Sharma <xplosiveunicorn11@gmail.com>
2022-10-31 20:56:38 +0530

    refactored code

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const noOfNotes = document.querySelectorAll(".no-of-notes");
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
-    
+
     hideMessage();
     if (billAmount.value > 0) {
         if (cashGiven.value >= billAmount.value) {
